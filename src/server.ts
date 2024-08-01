801ed07876daadd9970b50f70531d019384b11bd
GabrielRamison <gabriel.ramison@gmail.com>
2024-08-01 11:57:34 -0300

    Inicial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { app } from './app';
+import { env } from './env';
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP Server Running!");
+  })
+  .catch(err => {
+    console.error('Error starting server:', err);
+    process.exit(1);
+  });
