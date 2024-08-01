b9f98b76ee0c3eb39ce9bd496ff28888c38459c7
GabrielRamison <gabriel.ramison@gmail.com>
2024-08-01 15:16:21 -0300

    fix: bind server to 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import { app } from './app';
 import { env } from './env';
 
+const PORT = env.PORT || 3000;
+
 app
   .listen({
-    port: env.PORT,
+    port: PORT,
+    host: '0.0.0.0', // Adicione esta linha
   })
   .then(() => {
-    console.log("HTTP Server Running!");
+    console.log(`HTTP Server Running on port ${PORT}!`);
   })
   .catch(err => {
     console.error('Error starting server:', err);
